011dd3c0bd2151fc3a618cb8fb1c73dc0020f010
Rushi <rushighate128@gmail.com>
2025-10-28 20:19:09 +0530

    Update read_2FF.sv

--- rtl_file/read_2FF.sv
+++ rtl_file/read_2FF.sv
@@ -6,14 +6,19 @@ module read_2FF #(ADD_WIDTH = 3)
 	output reg [ADD_WIDTH:0] rd_2ff_sync;
 
 
-	//  this is the interna reg which work as 1 bit FF
+	//  this is the internal reg which works as 1 1-bit FF
 	reg [ADD_WIDTH:0] rd_2ff;  // reg 
 
 	always @(posedge rd_clk or negedge rd_rst) begin
+		
 		if(~rd_rst) begin
+			
             rd_2ff <= 0;
+			
 	end else begin
-	     {rd_2ff_sync, rd_2ff} <= {rd_2ff_in, rd_2ff};
+		
+		{rd_2ff_sync, rd_2ff} <= { rd_2ff_in, rd_2ff };
+		
    end
   end
  
